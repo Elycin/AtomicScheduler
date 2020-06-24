f6e0efab6d0f1bb132b9e6600f1836de71b2e7b1
Ely Haughie <ely@elyc.in>
2020-06-24 16:43:06 -0700

    Initial Commit

---
+++ Drivers/IAtomicDriver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicScheduler.Drivers
+{
+    interface IAtomicDriver
+    {
+        readonly static string Driver;
+
+        void Set(string key);
+        bool Has(string key);
+
+    }
+}
---
+++ Exceptions/ThreadNotRunningException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicScheduler.Exceptions
+{
+    class ThreadNotRunningException : Exception
+    {
+        public ThreadNotRunningException() : base() { }
+        public ThreadNotRunningException(string message) : base(message) { }
+    }
+}
